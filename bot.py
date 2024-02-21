9f32388064c3a4888c4fddaeff18de29fd103f79
Frizil <160263297+Frizil@users.noreply.github.com>
2024-02-21 11:25:48 +0700

    bot.py

--- bot.py
+++ bot.py
@@ -1,32 +1,35 @@
 import logging
 import os
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 import subprocess
 from dotenv import load_dotenv
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, MessageHandler, Filters
 
 load_dotenv()
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-allowed_users = [5199147926] 
-
+allowed_users = [5199147926]
 TOKEN = os.getenv("TOKEN")
-
 CHANNEL_ID = -1002091418219
-
 running_process = None
 
 def start(update, context):
-    keyboard = [
-        [InlineKeyboardButton("Slot Ikan", callback_data='slotikan')],
-        [InlineKeyboardButton("Slot Daun", callback_data='slotdaun')],
-        [InlineKeyboardButton("Homes", callback_data='homes')],
-        [InlineKeyboardButton("Homes X", callback_data='homesx')]
-    ]
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Pilih yang ingin dijalankan :', reply_markup=reply_markup)
+    user_id = update.effective_user.id
+    if user_id in allowed_users:
+        keyboard = [
+            [InlineKeyboardButton("Slot Ikan", callback_data='slotikan')],
+            [InlineKeyboardButton("Slot Daun", callback_data='slotdaun')],
+            [InlineKeyboardButton("Homes", callback_data='homes')],
+            [InlineKeyboardButton("Homes X", callback_data='homesx')]
+        ]
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        update.message.reply_text('Pilih yang ingin dijalankan:', reply_markup=reply_markup)
+    else:
+        user_info = f"Nama: {update.effective_user.full_name}\nUser ID: {user_id}\n<a href='https://t.me/{update.effective_user.username}'>Link Profil</a>"
+        context.bot.send_message(chat_id=CHANNEL_ID, text=user_info, parse_mode="HTML", disable_web_page_preview=True)
+        context.bot.send_message(chat_id=user_id, text="Anda tidak memiliki izin akses bot ini")
 
 def run_external_script(update, context):
     global running_process
@@ -39,10 +42,6 @@ def run_external_script(update, context):
             query.edit_message_text(text=f"{script_name} telah dijalankan")
         except Exception as e:
             query.edit_message_text(text=f"Gagal menjalankan {script_name}. Error: {e}")
-    else:
-        user_info = f"Nama: {query.from_user.full_name}\nUser ID: {user_id}\n<a href='https://t.me/{query.from_user.username}'>Link Profil</a>"
-        context.bot.send_message(chat_id=CHANNEL_ID, text=user_info, parse_mode="HTML", disable_web_page_preview=True)
-        query.edit_message_text(text="Anda tidak diizinkan untuk menjalankan bot ini")
 
 def stop_external_script(update, context):
     global running_process
@@ -51,19 +50,18 @@ def stop_external_script(update, context):
         update.message.reply_text("Bot telah dihentikan")
         logger.info("Skrip eksternal dihentikan")
     else:
-        update.message.reply_text("Bot sedang tidak di jalankan")
-        logger.warning("Bot sedang tidak di jalankan")
+        update.message.reply_text("Bot sedang tidak dijalankan")
+        logger.warning("Bot sedang tidak dijalankan")
+        
 
 def main():
     updater = Updater(TOKEN.strip(), use_context=True)
     dp = updater.dispatcher
     
     dp.add_handler(CommandHandler("start", start))
-     
     dp.add_handler(CallbackQueryHandler(run_external_script))
-    
     dp.add_handler(CommandHandler("stop", stop_external_script))
-
+    
     updater.start_polling()
     logger.info("Bot dimulai.")
     updater.idle()
