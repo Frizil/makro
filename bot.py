07d37e9a6f324ea6dbc7a0747f4f0783e1ddfd5c
Frizil <160263297+Frizil@users.noreply.github.com>
2024-04-09 22:39:49 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,6 @@ from os import getenv
 from aiogram import Bot, Dispatcher, types
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.dispatcher.filters import CommandStart, Command
 
 TOKEN = getenv("TOKEN")
 allowed_users = [5199147926]
