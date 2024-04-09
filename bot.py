a986cbf212dce5978e08e2f3dde655d8e0c357e0
Frizil <160263297+Frizil@users.noreply.github.com>
2024-04-09 22:43:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,10 @@ import sys
 from os import getenv
 
 from aiogram.filters import Filter
+from aiogram import Bot, Dispatcher, html
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart
 from aiogram.types import Message
 
 TOKEN = getenv("TOKEN")
