ce62e55bd1ab6bd53842ac30f16b17730c7ba167
Frizil <160263297+Frizil@users.noreply.github.com>
2024-04-09 23:16:05 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,12 @@
-
-import subprocess
-import os
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import CallbackContext, CommandHandler, CallbackQueryHandler, Updater
 
 from dotenv import load_dotenv
+import os
+import subprocess
 
 load_dotenv()
 
-# Mengambil token dari environment variable
 TOKEN = os.getenv("TOKEN")
 allowed_users = [5199147926]
 CHANNEL_ID = -1002091418219
@@ -42,7 +40,7 @@ def run_external_script(update: Update, context: CallbackContext):
     if user_id in allowed_users:
         script_name = context.callback_query.data
         try:
-            running_process = subprocess.Popen(["python", f"{script_name}.py"])
+            running_process = subprocess.Popen(["python3", f"{script_name}.py"])  # Ubah 'python' menjadi 'python3'
             update.callback_query.message.edit_text(text=f"{script_name} telah dijalankan")
         except Exception as e:
             update.callback_query.message.edit_text(text=f"Gagal menjalankan {script_name}. Error: {e}")
