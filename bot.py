9330b774c3f54ef7312fdc74922a03309f64f2d0
Frizil <160263297+Frizil@users.noreply.github.com>
2024-04-10 15:04:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, Message, ChatAction
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update, Message
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, CallbackContext, MessageHandler, Filters
 import logging
 import subprocess
